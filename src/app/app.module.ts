95c78dc879b48a567978775ed7219c1c20f6bfc3
Henri-joel Guissi <guissi.henrijoelzeze@bluecrest.edu.gh>
2022-12-13 07:13:11 +0000

    Initial Commit. Components, generationg the various components with ng generate command

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    CarouselComponent,
+    ProductItemComponent,
+    ProductDetailComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
